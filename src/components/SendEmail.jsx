1b8335a648632dbbad65409ab9ca48f8b91b8933
Sanskar Rathore <sanskarrathore1553+github@gmail.com>
2024-09-10 19:13:54 +0530

    updates
    - minor bug fixes
    - profile picture
    - firebase restored

--- src/components/SendEmail.jsx
+++ src/components/SendEmail.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import React, { useState } from 'react';
 import { RxCross2 } from "react-icons/rx";
 import { useDispatch, useSelector } from 'react-redux';
-import { setOpen } from '../redux/appSlice';
 import { db } from '../firebase';
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { setOpen } from '../redux/appSlice';
 
 const SendEmail = () => {
     const [formData, setFormData] = useState({
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
-import { initializeApp } from "firebase/app";
+// Import the functions you need from the SDKs you need
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database"; 
-import { GoogleAuthProvider,getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCCxklo4ehV_I5NwZWRkDReg46I-3Rpo5A",
-  authDomain: "clone-6dd4f.firebaseapp.com",
-  projectId: "clone-6dd4f",
-  storageBucket: "clone-6dd4f.appspot.com",
-  messagingSenderId: "781876226205",
-  appId: "1:781876226205:web:0d4463c344d70c0994cc74",
-  measurementId: "G-ZT3MWT0RX8"
+  apiKey: "AIzaSyCpqo0Frz6bPgUDyrBX6fXuOj31x902B08",
+  authDomain: "fullstack-g-mail-15.firebaseapp.com",
+  projectId: "fullstack-g-mail-15",
+  storageBucket: "fullstack-g-mail-15.appspot.com",
+  messagingSenderId: "997723899957",
+  appId: "1:997723899957:web:c98cd2442a6dbde876497c",
+  measurementId: "G-FPN3BCEGNP"
 };
 
 // Initialize Firebase
@@ -20,3 +22,8 @@ const analytics = getAnalytics(app);
 export const auth = getAuth();
 export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
+
+
+
+
+
